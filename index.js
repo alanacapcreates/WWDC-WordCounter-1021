3d6df0f39a867da2bd93c8babefac8eb59493eb0
Alana Caporale <38472545+alanacapcreates@users.noreply.github.com>
2021-10-18 11:28:51 -0400

    import from Scrimba

---
+++ index.js
@@ -0,0 +1,56 @@
+// Tasks: 
+
+// - write the JS so that the word count appears on the screen
+// - update the count as the user types
+
+
+
+var textarea = document.querySelector('textarea');
+var p = document.querySelector('p');
+var arr = []
+let countEl= 0
+
+
+textarea.addEventListener('input', function (event) {
+  arr = event.target.value.split(" ").filter((w) => w.length > 0);
+ 
+    
+        //empty state
+        if(event.target.value==""){
+        longestWord()
+        p.innerText =`Word count: 0, Longest word: ` 
+        countEl=countEl  
+        }
+        else {
+            longestWord()
+            countEl=arr.length
+            p.innerText=`Word count: ${countEl}, Longest word: ${longestWord()}`
+        }
+    //   console.log(`Split Array: ${arr}`)
+
+  
+
+});
+
+function longestWord(){
+    let wordMap = arr.map(w => w.length)
+    let longestWord = ""
+    
+    console.log(`Word Map Count: ${wordMap}`)
+    if(arr.length <=1){
+        longestWord = arr[0]
+    
+    }else{
+        for(i=1;i<wordMap.length;i++){
+            if(wordMap[i]>wordMap[i-1]){
+                longestWord = arr[i]
+            }else if(wordMap[i]<wordMap[i-1]){
+            longestWord = arr[i-1]
+            }else if(wordMap[i]==wordMap[i-1]){
+            longestWord = arr[i-1]
+            }
+        console.log(`Longest Word: ${longestWord}`)
+        }
+    }
+  return longestWord
+}
